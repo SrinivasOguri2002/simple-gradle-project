9a6b9f61ddfd5422a9dc5dd749d9ffa35b5d0e89
SrinivasOguri2002 <voguri@asu.edu>
2025-04-01 15:28:43 -0700

    Initial commit

---
+++ src/test/java/com/example/SecondTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SecondTest {
+    @Test
+    void testAlsoTrue() {
+        assertTrue(true);
+    }
+}
